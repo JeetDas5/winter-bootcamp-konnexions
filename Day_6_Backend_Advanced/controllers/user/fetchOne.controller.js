f36e86d7621dad6e47bbc602b51e7bb621679e91
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-30 23:02:08 +0530

    notes

--- Day_6_Backend_Advanced/controllers/user/fetchOne.controller.js
+++ Day_6_Backend_Advanced/controllers/user/fetchOne.controller.js
@@ -3,6 +3,8 @@ import fetchOneUserService from "../../services/user/fetchOne.service.js";
 
 const fetchOneUserController = async (req, res, next) => {
     try {
+        // /:id ==> id is in req.param
+        // ?id=123 ==> id is in req.query
         const { id: userId } = req.params;
         if (!isValidObjectId(userId)) {
             return res.status(400).json({ success: false, message: 'Invalid user ID' });
